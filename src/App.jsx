fb74a8cf01db1f8dfe622b70fcaa7971b79c7496
Joaquin Acosta <joaquinacosta397@outlook.com>
2023-09-22 10:24:21 -0300

    asd

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,11 @@ const TURNS = {
   O: 'o'
 }
 
-
 const Square = ({ isSelected, children, updateBoard, index}) => {
   const className = `square ${isSelected ? 'is-selected' : '' }`
 
   const handleClick = () => {
-    updateBoard()
+    updateBoard(index)
   }
   return (
     <div className={className} onClick={handleClick}>
@@ -19,16 +18,35 @@ const Square = ({ isSelected, children, updateBoard, index}) => {
   )
 }
 
+const WINNER_COMBOS = [
+  [ 0, 1, 2],
+  [ 3, 4, 5],
+  [ 6, 7, 8],
+  [ 0, 3, 6],
+  [ 1, 4, 7],
+  [ 2, 5, 8],
+  [ 0, 4, 8],
+  [ 2, 4, 6]
+]
+
 function App() {
 
 const [ board, setBoard] = useState(Array(9).fill(null))
 
 const [ turn, setTurn ] = useState(TURNS.X)
+const [ winner, setWinner ] = useState(null)
   
-const updateBoard = () => {
+const updateBoard = (index) => {
+  if(board[index]) return
+
+  const newBoard = [...board]
+  newBoard[index] = turn
+  setBoard(newBoard)
+
   const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
   setTurn(newTurn)
 }
+
   return (
     <main className='board'> 
       <h1>Tic Tac Toe</h1>
